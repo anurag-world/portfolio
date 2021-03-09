8d61e3358e6ca01c736d385de6584d9355c9cb4a
Anurag World <47092363+anurag-world@users.noreply.github.com>
2021-03-10 01:28:03 +0530

    update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { LinkContainer } from 'react-router-bootstrap'
 
 const Navigation = (props) => {
   return (
@@ -10,8 +9,8 @@ const Navigation = (props) => {
           {props.name}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="navbarSupportedContent">
-          <Nav className="ms-auto">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <NavDropdown title="Portfolio" id="navbarDropdown">
               <NavDropdown.Item href="/ui">UI Design</NavDropdown.Item>
